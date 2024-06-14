77f3777fbf17a2e4130c331921a8c08de057d6b3
Alex Olivier <alex@alexolivier.me>
2024-06-14 09:42:22 +0100

    revert
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,29 +1,7 @@
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "res.cloudinary.com", // cloudinary images
-            "lh3.googleusercontent.com", // google avatars
-            "platform-lookaside.fbsbx.com", // facebook avatars
-            "dl.airtable.com", // strapi marketplace
-            `https://cerbos.dev`,
-          ],
-          "media-src": ["'self'", "data:", "blob:", `https://cerbos.dev`],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  "strapi::security",
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
